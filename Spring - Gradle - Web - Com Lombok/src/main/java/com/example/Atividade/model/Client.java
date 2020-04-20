b706c62301b09031dc524f28c300d42ec535cc99
Harvey Braun <63766520+harveybraun@users.noreply.github.com>
2020-04-19 21:13:42 -0300

    Add files via upload

--- Spring - Gradle - Web - Com Lombok/src/main/java/com/example/Atividade/model/Client.java
+++ Spring - Gradle - Web - Com Lombok/src/main/java/com/example/Atividade/model/Client.java
@@ -2,6 +2,13 @@ package com.example.Atividade.model;
 
 import lombok.*;
 
+/**
+ * Descreve a Classe Client
+ *
+ * @author Harvey Braun
+ * @version 1.0
+ * @since 19/04/2020 - 15:00
+ */
 @ToString
 @NoArgsConstructor @AllArgsConstructor
 @Getter @Setter
